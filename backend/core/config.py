080c2f35406070c57dfbcd5e7bbb643f6eb11019
gleb <deffender1337@gmail.com>
2025-02-22 15:42:45 +0300

    db config

---
+++ backend/core/config.py
@@ -0,0 +1,32 @@
+from pydantic_settings import BaseSettings,SettingsConfigDict
+from pydantic import BaseModel
+
+class DatabaseSettings(BaseSettings):
+    db_name: str
+    db_user: str
+    db_pass: str
+    db_host: str
+    db_port: int
+    
+    test_db_name: str
+    test_db_user: str
+    test_db_pass: str
+    test_db_host: str
+    test_db_port: int
+
+    def get_asyncpg_url(self, test: bool = False) -> str:
+        if test:
+            return f"postgresql://{self.test_db_user}:{self.test_db_pass}@{self.test_db_host}:{self.test_db_port}/{self.test_db_name}"
+        return f"postgresql://{self.db_user}:{self.db_pass}@{self.db_host}:{self.db_port}/{self.db_name}"
+
+
+class ApiPrefix(BaseSettings):
+    prefix: str = '/api'
+
+class Settings(BaseSettings):
+    api: ApiPrefix = ApiPrefix()
+    db: DatabaseSettings = DatabaseSettings()
+
+
+settings = Settings()
+print(settings.db)
--- backend/main.py
+++ backend/main.py
@@ -1,3 +1,12 @@
 from fastapi import FastAPI
 
+from core.config import settings
+from api import router as api_router
 app = FastAPI()
+app.include_router(api_router,
+                   prefix=settings.api.prefix)
+
+@app.get('/')
+def test():
+    return settings.db
+    
