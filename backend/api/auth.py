0fb7b401b4166d99fc3af89779c45303561cd8f0
gleb <deffender1337@gmail.com>
2025-02-25 08:02:49 +0300

    rm repo code

--- backend/api/auth.py
+++ backend/api/auth.py
@@ -1,15 +1,10 @@
 from fastapi import APIRouter
-from repositories.users import sql_user_repository
-
-from uuid import uuid4
 
 router = APIRouter(prefix='/auth',tags=['Authentication'])
 
 @router.post('/login')
 async def login():
-    data = {'username' : '22', 'password' : '22'}
-    await sql_user_repository.create(data)
-    return data
+    return {}
 
 @router.post('/register')
 async def register():
