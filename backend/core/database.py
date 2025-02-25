a1df1d80817177cbff731ca0447ad17bc8027732
gleb <deffender1337@gmail.com>
2025-02-25 08:14:17 +0300

    refactor: remove unused code and clean up imports

--- backend/core/database.py
+++ backend/core/database.py
@@ -3,7 +3,6 @@ from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 from core.config import settings
 
-
 Base = declarative_base()
 
 class Database:
