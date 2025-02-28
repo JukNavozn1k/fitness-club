35921c9891b3fc0930d2df56cfa84fe1ff5a1dd7
gleb <deffender1337@gmail.com>
2025-02-28 21:32:41 +0300

    exception handling (login)

--- backend/api/auth.py
+++ backend/api/auth.py
@@ -1,16 +1,20 @@
-from fastapi import APIRouter,HTTPException
+from fastapi import APIRouter,HTTPException,status
 
-
-from schemas.auth import AuthSchema
+from schemas.auth import AuthSchema,TokenSchema
 from schemas.users import UserSchema
 
 from services.users import user_service
 
 router = APIRouter(prefix='/auth',tags=['Authentication'])
 
-@router.post('/login')
+@router.post('/login',response_model=TokenSchema)
 async def login(schema: AuthSchema):
     token = await user_service.login(schema.model_dump())
+    if token is None:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid credentials or failed to generate token"
+        )
     return token
 
 @router.post('/register',response_model=UserSchema)
--- backend/schemas/auth.py
+++ backend/schemas/auth.py
@@ -3,3 +3,7 @@ from pydantic import BaseModel
 class AuthSchema(BaseModel):
     username: str
     password: str
+
+class TokenSchema(BaseModel):
+    token: str
+    type: str
