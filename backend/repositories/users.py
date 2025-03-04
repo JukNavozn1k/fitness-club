5536d65d6eeba7bdce17722fea42e7f6b5b9e7aa
gleb <deffender1337@gmail.com>
2025-03-04 22:47:06 +0300

    feat: implement retrieve_by_username method in UserSQLRepository

--- backend/repositories/users.py
+++ backend/repositories/users.py
@@ -4,10 +4,10 @@ from models.database import db
 
 
 class UserSQLRepository(AbstractSQLRepository):
-    # async def retrieve_by_username(self, username: str):
-    #     res = await self.retrieve_by_field('username', username)
-    #     return res
-    ...
+    async def retrieve_by_username(self, username: str):
+        res = await self.retrieve_by_field('username', username)
+        return res
+
 
 
 sql_user_repository = UserSQLRepository(db.get_session, User)
