d8d016cb16dc90767d03e961489b5b338ce29ef3
gleb <deffender1337@gmail.com>
2025-03-13 17:53:35 +0300

    removed userCreate schema

--- backend/schemas/users.py
+++ backend/schemas/users.py
@@ -5,9 +5,6 @@ from typing import List
 class UserBase(BaseModel):
     username: str
 
-class UserCreate(UserBase):
-    password: str
-
 class UserOut(UserBase):
     id: int
     roles: List['RoleOut'] = []
