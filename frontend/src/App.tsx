8b7bb6c24da57947faed932a4128373b471be443
gleb <deffender1337@gmail.com>
2025-03-16 00:33:50 +0300

    login form!

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,7 +3,11 @@ import LoginForm from "./pages/login"
 function App() {
   return (
     <>
-      <LoginForm/>
+     <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-br from-blue-50 to-blue-100 p-4">
+      <div className="w-full max-w-md">
+        <LoginForm />
+      </div>
+    </main>
     </>
   )
 }
