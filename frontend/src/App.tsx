8eb0994c312d0ffb5f5d620bb5d8afab2c1be58d
gleb <deffender1337@gmail.com>
2025-03-16 17:54:58 +0300

    Remove unused App.css file and update App component structure for improved layout

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,15 @@
 import { BrowserRouter } from 'react-router-dom'
-import './App.css'
+
 
 import Home from './pages/Home'
 
 function App() {
- 
-
   return (
-    <>
-      <BrowserRouter>
-        <Home/>     
-      </BrowserRouter>
-    </>
+    <BrowserRouter>
+      <div className="min-h-screen flex flex-col">
+        <Home />     
+      </div>
+    </BrowserRouter>
   )
 }
 
