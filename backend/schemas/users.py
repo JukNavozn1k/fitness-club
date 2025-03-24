a65e17b85bf6f4c9df2c2370dd8a1cca49153dec
gleb <deffender1337@gmail.com>
2025-03-24 19:11:45 +0300

    refactor: simplify user role handling by removing enum definitions and using a string type

--- backend/schemas/users.py
+++ backend/schemas/users.py
@@ -1,20 +1,5 @@
 from pydantic import BaseModel
-
-from enum import Enum
 from datetime import datetime
-
-# Перечисление ролей
-class RoleEnum(str, Enum):
-    admin = "admin"
-    trainer = "trainer"
-    member = "member"
-
-# Перечисление разрешений
-class PermissionEnum(str, Enum):
-    create_workout = "create_workout"
-    delete_workout = "delete_workout"
-    update_workout = "update_workout"
-    view_workout = "view_workout"
 
 # Схемы для User
 class UserBase(BaseModel):
@@ -22,7 +7,7 @@ class UserBase(BaseModel):
 
 class UserOut(UserBase):
     id: int
-    role: RoleEnum = RoleEnum.member
+    role: str
     joined_date: datetime
 
     class Config:
