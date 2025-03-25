3a3b615fc9164197665d8ad58da3eec2f059ca98
gleb <deffender1337@gmail.com>
2025-03-26 02:12:06 +0300

    Как всё задолбало...

--- backend/models/reviews.py
+++ backend/models/reviews.py
@@ -1,14 +1,14 @@
 
-from beanie import Document,Link
-from .users import UserMongo
+from beanie import Document
+
 from datetime import datetime
-from beanie import Document,Indexed
+from beanie import Document
 
 from pydantic import Field
 
 
 class ReviewMongo(Document):
-    user: Link[UserMongo]  # Связь с пользователем
+   
     rating: int = Field(..., ge=1, le=5)  # Оценка от 1 до 5
     comment: str = Field(default="")
     created_at: datetime = Field(default_factory=datetime.utcnow)
--- backend/schemas/users.py
+++ backend/schemas/users.py
@@ -1,10 +1,7 @@
 from pydantic import BaseModel
-from beanie import PydanticObjectId
 from datetime import datetime
 
-
-
 class UserOut(BaseModel):
-    id: PydanticObjectId
+
     username: str
     joined_date: datetime
