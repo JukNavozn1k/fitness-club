e88e3250c6c4e62cd55bfe95d0f0efc9fc7525c0
gleb <deffender1337@gmail.com>
2025-03-26 20:13:26 +0300

    update review!

--- backend/services/reviews.py
+++ backend/services/reviews.py
@@ -2,15 +2,22 @@ from repositories import reviews_repository
 class ReviewsService:
     def __init__(self,repository):
         self.repository = repository
+    
+
+    async def get_user_review(self, user: dict):
+        return await self.repository.retrieve_by_field(field_name='user.$id',value=user['id'],populate=['user'])
 
     async def add_review(self, data):
-        return await self.repository.add_review(data)
+        user_rewiew = await self.get_user_review(data['user'])
+        if not user_rewiew:
+            return await self.repository.add_review(data)
+        else: 
+            return await self.repository.update(pk=user_rewiew['id'],data=data, populate=['user'])
     
     async def get_reviews(self):
         return await self.repository.list(populate=['user'])
     
-    async def get_user_review(self, user: dict):
-        return await self.repository.retrieve_by_field(field_name='user.$id',value=user['id'],populate=['user'])
+    
 
 def get_reviews_service():
     return ReviewsService(reviews_repository)
