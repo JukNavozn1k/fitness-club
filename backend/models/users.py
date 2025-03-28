a038b5c0500b2b0f693d2118ec70ad0df6c973d4
gleb <deffender1337@gmail.com>
2025-03-28 05:01:59 +0300

    Add RBAC configuration and refactor user model for role management

--- backend/models/users.py
+++ backend/models/users.py
@@ -4,7 +4,11 @@ from .database import Base
 
 from datetime import datetime
 
-from beanie import Document,Indexed
+from beanie import Document,Indexed,Link
+from typing import List
+
+from enum import Enum
+
 
 from pydantic import Field
 
@@ -17,13 +21,27 @@ class UserSQL(Base):
    
     joined_date: Mapped[datetime] = mapped_column(DateTime, default=datetime.utcnow)
 
+class Permission(str, Enum):
+    CREATE_USER = "create_user"
+    DELETE_USER = "delete_user"
+    EDIT_ROLES = "edit_roles"
+    VIEW_USERS = "view_users"
+
+
+
+class RoleMongo(Document):
+    name: Indexed(str, unique=True)  # type: ignore
+    permissions: List[Permission]
+    
+    class Settings:
+        name = "roles"
 
 class UserMongo(Document):
     username: Indexed(str, unique=True) = Field(...)
     password: str = Field(...)
 
     joined_date: datetime = Field(default_factory=datetime.utcnow)
-
+    roles : List[Link[RoleMongo]] = []
 
     class Settings:
         name = "users"
